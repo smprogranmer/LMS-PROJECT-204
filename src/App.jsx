1a6de205fc5b06ba0adfd09a9ed4a124ac5934f0
siyam <s.mprogrammer788@gmail.com>
2024-05-10 04:32:36 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Spline from "@splinetool/react-spline";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      LMS PROJECT 2024
+    </div>
+  );
+}
+
+export default App;
